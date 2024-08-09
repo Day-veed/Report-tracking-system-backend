e89d42132a2cc1dc8c7bdc8425e93691b8771434
Day-veed <davidadekunle9@gmail.com>
2024-08-09 20:50:00 +0100

    initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 const authRoutes = require('./routes/auth');
 const reportRoutes = require('./routes/report');
 
-//. Connect to MongoDB
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
