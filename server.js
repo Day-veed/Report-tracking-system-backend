a20267d5acabe46a9d6d8d9c8b9d5325f6f19f05
Day-veed <davidadekunle9@gmail.com>
2024-08-11 16:22:13 +0100

    linking server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// Route middlewares
+//. Route middlewares
 app.use('/api/auth', authRoutes);
 app.use('/api/report', reportRoutes);
 
